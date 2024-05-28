4c00c44f1fd4fd4f469514efa3d215211988d188
khushal vora <vorakhushal482@gmail.com>
2024-05-28 14:38:18 +0530

    add

--- db.js
+++ db.js
@@ -1,22 +1,12 @@
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://datanadminuser:jnaQTVd2mbQ3n5@195.201.195.230:27017/?authMechanism=DEFAULT';
-
+var url = 'mongodb://datanadminuser:jnaQTVd2mbQ3n5@195.201.195.230:27017/?authMechanism=DEFAULT';
 let db = null;
+try{
+const client = new MongoClient(url);
+client.connect();
+db = client.db('dataanant');
+} catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  } 
 
-async function connectToDatabase() {
-    try {
-        const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-        await client.connect();
-        db = client.db('dataanant');
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-        throw error;
-    }
-}
-
-connectToDatabase();
-
-module.exports = {
-    collection: (name) => db.collection(name),
-};
+	module.exports = db;
